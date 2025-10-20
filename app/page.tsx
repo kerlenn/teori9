44ce3fd6e1717fca866aff70919760ef6b00772d
kerlenn <kerlen.535240109@stu.untar.ac.id>
2025-10-20 21:10:43 +0700

    ganti tema

--- app/page.tsx
+++ app/page.tsx
@@ -104,51 +104,6 @@ export default function Home() {
       <Navbar />
       <div className={styles.page}>
         <main className={styles.main}>{renderPage()}</main>
-
-        <footer className={styles.footer}>
-          <a
-            href="https://nextjs.org/learn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
       </div>
     </>
   );
